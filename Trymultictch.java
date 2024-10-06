f282f3292c91591ad69d68b33567e03ee4c9c487
DEEPAK LAKHAMAPURKAR <142570416+Deepak22112004@users.noreply.github.com>
2024-10-06 14:27:15 +0530

    Add files via upload

---
+++ Trymultictch.java
@@ -0,0 +1,17 @@
+class Trackmulticatch{
+	public static void main(String args[]){
+		int a=10,b=10,c,d[]={10,20,30};
+		try{
+			c=a/b;
+			System.out.print(c);
+			System.out.println(d[3]);
+		}
+		catch(ArithmeticException e){
+			System.out.println(e);
+		}
+		catch(ArrayIndexOutOfBoundsException e){
+			System.out.println(e);
+		}
+		System.out.println("bye");
+	}
+}
